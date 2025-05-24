92c290123453c677fc922d977c481dd932e16e75
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-05-24 20:44:10 +0600

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,32 @@
+# User defined function
+
+''' syntax --   def function_name():
+                    operation
+                    print / return
+'''
+def add(a, b):
+    plus = a + b
+    print(plus)
+
+
+def subtract(a, b):
+    sub = a - b
+    print(sub)
+
+def multiply(a, b):
+    multi = a * b
+    print(multi)
+
+def divide(a, b):
+    dive = a / b
+    print(dive)
+
+
+
+add(10, 20)
+
+subtract(10, 20)
+
+multiply(5, 125)
+
+divide(10, 2)
