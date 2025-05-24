865a2147df44d9a4079ecffe3ed9d66b1629acca
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-05-24 20:20:28 +0600

    Add files via upload

---
+++ IF ELSE.py
@@ -0,0 +1,14 @@
+
+a= 10
+b= 20
+
+if a==b :
+    print("A and B are equal")
+elif a>b :
+    print("A is greater than B")
+elif a<b :
+    print("A is less than B")
+elif a<=b :
+    print("A is less than or equal to B")
+else:
+    print("A is not equal to B")
