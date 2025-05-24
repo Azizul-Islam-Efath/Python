c35b10f8f8993c9c72f637ebd8e20da7dc260bbb
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-05-24 20:28:45 +0600

    Add files via upload

---
+++ Loops in python.py
@@ -0,0 +1,17 @@
+
+
+# while loop
+
+i = 1
+while i <= 4:
+    print(f"{i} .I AM PYTHON")
+    i=i+1
+
+# for loop
+
+name = ["efath","mishka","Maria","Sumaiyea"]
+
+for i in name:
+    if i== "Maria":
+        break
+    print(i)
