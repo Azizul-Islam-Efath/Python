edf5c5f5b9f5468345fc87ff49ea5d5df20645a6
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-05-31 21:16:06 +0600

    Add files via upload

---
+++ ARRAY.py
@@ -0,0 +1,7 @@
+
+array = [1,2,3,4,5,6,7,8,9]
+
+print(f"INITIALLY ARRAY: {array} ")
+
+array[0]="AZIZUL"
+print(f"AFTER CHANGING INDEX ARRAY: {array} ")
---
+++ CLASSES & OBJECT.py
@@ -0,0 +1,11 @@
+
+#class
+
+class Info:
+    Name= ""
+    Number= ""
+
+a=Info()   # object creation
+a.Name= "AZIZUL ISLAM"
+
+print(a.Name)
