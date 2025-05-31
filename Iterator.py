fce29f89bbc9ed67f7576ac0308749823a061105
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-05-31 21:50:33 +0600

    Add files via upload

---
+++ Iterator.py
@@ -0,0 +1,16 @@
+
+
+# creating list
+
+List = [1,2,5,6,"Efath"]
+
+#for i in List:
+#    print(i)
+
+it = iter(List)  # calling iter function
+print(next(it))  # Iterating using next method  next(variable name)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
