c3002c3272ca07cc2cbc85efa0212fe7cf3ba372
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-06-01 20:37:48 +0600

    Add files via upload

---
+++ Math functions.py
@@ -0,0 +1,74 @@
+
+
+# min function
+
+a= min(10,2,5,6,8,7,15)
+print("The lowest number is :",a)
+
+# max function
+
+b= max(10,2,5,6,8,7,15)
+print("The highest number is :",b)
+
+# convert negative value to positive using abs function
+
+c= abs(-125)
+print("The positive value is :",c)
+
+# Power function
+
+d=pow(10,3)     # pow(value,exponent)
+print("The value of 10^3 is :",d)
+
+
+# importing math module
+import math
+
+# Square root function which returns the value of sqrt of a number
+
+e=math.sqrt(81)  # calling the sqrt function using math module which is imported
+print("Square root of 81 is :",e)
+
+# Ceil and floor method
+# The math.ceil() method rounds a number upwards to its nearest integer,
+# the math.floor() method rounds a number downwards to its nearest integer
+
+f= math.ceil(1.5)
+print("The ceil value is :",f)
+
+g=math.floor(1.5)
+print("The floor value is :",g)
+
+# pi function
+
+h= math.pi   # returns the absolute value of pie
+print("The pi value is :",h)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
