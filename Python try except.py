c748f03d39ccbdfb42e8c5d27709f30e3d20b5ac
Azizul-Islam-Efath <azizulislam5360@gmail.com>
2025-06-02 22:11:54 +0600

    Add files via upload

---
+++ Python try except.py
@@ -0,0 +1,11 @@
+
+
+# Try except  same as EXCEPTION HANDLING OF JAVA
+
+
+try:
+    print("I am in try block")
+except:
+    print("I am in except block")  # this line has an error
+
+print("After error block")
